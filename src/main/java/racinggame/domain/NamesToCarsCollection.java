23ebe66a06c73cf9f52bf66eb9ef7c47b5220f35
Beenie93 <seongbeen93@naver.com>
2021-10-11 01:53:11 +0900

    feat : 이름 목록이 비어있거나 null일 경우에 대한 예외 케이스 추가

--- src/main/java/racinggame/domain/NamesToCarsCollection.java
+++ src/main/java/racinggame/domain/NamesToCarsCollection.java
@@ -8,7 +8,14 @@ public class NamesToCarsCollection implements CarsCollection {
     private final List<String> names;
 
     public NamesToCarsCollection(final List<String> names) {
+        validateNullOrEmpty(names);
         this.names = new ArrayList<>(names);
+    }
+
+    private void validateNullOrEmpty(final List<String> names) {
+        if (names == null || names.isEmpty()) {
+            throw new IllegalArgumentException("하나 이상의 이름이 존재해야합니다.");
+        }
     }
 
     @Override
--- src/test/java/racinggame/domain/NamesToCarsCollectionTest.java
+++ src/test/java/racinggame/domain/NamesToCarsCollectionTest.java
@@ -2,11 +2,14 @@ package racinggame.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.assertj.core.api.Assertions.tuple;
 
 class NamesToCarsCollectionTest {
@@ -28,5 +31,15 @@ class NamesToCarsCollectionTest {
                 .extracting("name", "position")
                 .containsExactly(tuple(name1, position),
                         tuple(name2, position));
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("null 또는 비어있을 경우, 예외가 발생한다.")
+    void create_fail(List<String> names) {
+        //when, then
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new NamesToCarsCollection(names))
+                .withMessage("하나 이상의 이름이 존재해야합니다.");
     }
 }
