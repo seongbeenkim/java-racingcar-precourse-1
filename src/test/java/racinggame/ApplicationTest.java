39317ee82a042849e1e39196f1de6094fa9e13d0
Beenie93 <seongbeen93@naver.com>
2021-10-11 23:44:24 +0900

    test : 2대 미만일 경우에 대한 예외 처리 케이스 추가

--- src/test/java/racinggame/ApplicationTest.java
+++ src/test/java/racinggame/ApplicationTest.java
@@ -32,6 +32,14 @@ public class ApplicationTest extends NSTest {
         });
     }
 
+    @Test
+    void 자동차_2대_미만에_대한_예외_처리() {
+        assertSimpleTest(() -> {
+            runNoLineFound("pobi");
+            verify(ERROR_MESSAGE);
+        });
+    }
+
     @AfterEach
     void tearDown() {
         outputStandard();
