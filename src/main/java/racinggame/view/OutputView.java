502154b545f68e826996365718675a086c1fabcf
Beenie93 <seongbeen93@naver.com>
2021-10-11 23:27:00 +0900

    feat : 자동차 경주 결과 및 우승자 이름 출력 기능 구현

---
+++ src/main/java/racinggame/view/OutputView.java
@@ -0,0 +1,47 @@
+package racinggame.view;
+
+import racinggame.controller.CarsDto;
+import racinggame.controller.WinnersDto;
+
+import java.util.List;
+
+public class OutputView {
+
+    private static final String WINNER_NAME_DELIMITER = ", ";
+    private static final String POSITION_UNIT = "-";
+
+    public void printError(final String message) {
+        System.out.println("[ERROR] : " + message);
+    }
+
+    public void printPlayResult() {
+        System.out.println("실행 결과");
+    }
+
+    public void printRoundResult(final CarsDto carsDto) {
+        List<String> names = carsDto.getNames();
+        List<Integer> positions = carsDto.getPositions();
+
+        for (int i = 0; i < names.size(); i++) {
+            System.out.println(String.format("%s : %s", names.get(i), convertPositionValue(positions.get(i))));
+        }
+
+        System.out.println();
+    }
+
+    private String convertPositionValue(final Integer position) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i < position; i++) {
+            stringBuilder.append(POSITION_UNIT);
+        }
+
+        return stringBuilder.toString();
+    }
+
+    public void printWinners(final WinnersDto winnersDto) {
+        List<String> winnerNames = winnersDto.getNames();
+        String joinedNames = String.join(WINNER_NAME_DELIMITER, winnerNames);
+        System.out.println(String.format("최종 우승자는 %s 입니다.", joinedNames));
+    }
+}
